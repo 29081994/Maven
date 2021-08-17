d493cf57e30e76bc4aaa47e88b056ce020366d4e
mayur <mayursawanttalaulikar53@gmail.com>
2021-08-17 17:31:01 +0530

    first commit

---
+++ src/test/java/stepDefinations/StepDefinition.java
@@ -0,0 +1,39 @@
+package stepDefinations;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class StepDefinition {
+
+    @Given("^User is on Bank landing page$")
+    public void user_is_on_bank_landing_page() throws Throwable {
+    	System.out.println("Landing page");
+       
+    }
+
+    @When("^User login with valid username and valid password$")
+    public void user_login_with_valid_username_and_valid_password() throws Throwable {
+    	System.out.println("username and password");
+    }
+    
+    @Then("^user is navigated to homepage$")
+    public void user_is_navigated_to_homepage() throws Throwable {
+    	System.out.println("navigated to homepage");
+    }
+
+    @And("^cards are visible$")
+    public void cards_are_visible() throws Throwable {
+    	System.out.println("cards details are visible");
+    }
+
+}
+
+    
+ 
+
---
+++ target/test-classes/features/Login.feature
@@ -0,0 +1,7 @@
+Feature:  Bank Application
+
+Scenario: Home page of Application
+Given User is on Bank landing page
+When User login with valid username and valid password
+Then user is navigated to homepage
+And cards are visible
